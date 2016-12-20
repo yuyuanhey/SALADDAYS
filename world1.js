7ea5c103258f286a6738c8533c038fe05d9e0e78
yuyuanhey <cook19960626@gmail.com>
2016-12-20 11:56:37 +0800

    label

--- world1.js
+++ world1.js
@@ -4,27 +4,41 @@ var test1 = {"test": [{
     "count": 1
   }]};
 var color = ["rgba(255, 0, 0, 1)", "rgba(255, 0, 0, 0.7)", "rgba(255, 0, 0, 0.5)", "rgba(255, 0, 0, 0.3)", "rgba(255, 0, 0, 0.1)"]
+var colors = d3.scale.category20b();
+var dataset = d3.csv("C:/Users/user/desktop/testWorld.csv",function(d){console.log(d);});
 var basic_choropleth = new Datamap({
 		element: document.getElementById("container"),
+		geographyConfig: {
+    		highlightBorderColor: '#bada55',
+   			popupTemplate: function(geography, data) {
+   				if(data > 0) 
+     			return '<div class="hoverinfo">' + geography.properties.name
+     			+ " " +  data.count + ' ';
+     			else return '<div class="hoverinfo">' + geography.properties.name
+     			+ " " +  0 + ' ';
+     		},
+    		highlightBorderWidth: 3
+  		},
 	  // projection: 'mercator',
 		fills: {
-	  	defaultFill: color[4],
+		AFGcolor: colors(2),
+	  	defaultFill: colors(1),
 	  	authorHasTraveledTo: color[1],
 	  	try: "#ccc"
 		},
 		data: {
-			AFG: { fillKey: "defaultFill"},
-			ALB: { fillKey: "defaultFill"},
-			DZA: { fillKey: "defaultFill"},
-			AND: { fillKey: "defaultFill"},
-			AGO: { fillKey: "authorHasTraveledTo"},
-			ARG: { fillKey: "authorHasTraveledTo"},
-			ARM: { fillKey: "authorHasTraveledTo"},
-			AIA: { fillKey: "authorHasTraveledTo"},
-			AUS: { fillKey: "authorHasTraveledTo"},
-			AZE: { fillKey: "authorHasTraveledTo"},
-			BHS: { fillKey: "authorHasTraveledTo"},
-			BHR: { fillKey: "authorHasTraveledTo"}
+			"AFG": { "fillKey": "AFGcolor", "count": 1}
+			// ALB: { fillKey: "defaultFill"},
+			// DZA: { fillKey: "defaultFill"},
+			// AND: { fillKey: "defaultFill"},
+			// AGO: { fillKey: "authorHasTraveledTo"},
+			// ARG: { fillKey: "authorHasTraveledTo"},
+			// ARM: { fillKey: "authorHasTraveledTo"},
+			// AIA: { fillKey: "authorHasTraveledTo"},
+			// AUS: { fillKey: "authorHasTraveledTo"},
+			// AZE: { fillKey: "authorHasTraveledTo"},
+			// BHS: { fillKey: "authorHasTraveledTo"},
+			// BHR: { fillKey: "authorHasTraveledTo"}
 
 			/*d : {fillKey : "try"}
 			USA: { fillKey: "authorHasTraveledTo" },
